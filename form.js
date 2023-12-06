a5e3590be7f227eb1d3079347061a3dc6ed995c3
qDurush <145829009+qDurush@users.noreply.github.com>
2023-12-06 21:01:53 +0400

    Add files via upload

---
+++ form.js
@@ -0,0 +1,31 @@
+
+let nameinput = document.getElementById("fname");
+if (nameinput !==""){
+    alert("ad yazilib")
+}
+else{
+    alert("ad yoxdur")
+}
+let surnameinput = document.getElementById("surname");
+if (surnameinput !==""){
+    alert("soyad yazilib")
+}
+else{
+    alert("soyad yoxdur")
+}
+let emailinput = document.getElementById("email");
+if (emailinput !==""){
+    alert("email yazilib")
+}
+else{
+    alert("email yoxdur")
+}
+let passwordinput = document.getElementById("password");
+
+
+if (passwordinput !==""){
+    alert("sifre yazilib")
+}
+else{
+    alert("sifre yoxdur")
+}
